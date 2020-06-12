4960eb9f40ee2aaedd86fcfb8f6d590257ec8cb5
Rodrigo Britto Calovi <rodrigo-calovi@uergs.edu.br>
2020-06-12 14:24:45 -0300

    contador

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
+'use strict'
 
-    alert('Olá Mundo!');
+var maxLength = 150;
+
+function contaCaractere(){
+    let text = document.getElementById('txtMensagem');
+    let contador = maxLength - text.value.length;
+
+
+    let caracteres = document.getElementById('caracteres');
+    caracteres.innerText = contador;
+
+    if(contador<0){
+        caracteres.style.color = 'red';
+        text.style.border = '3px solid red';
+    }
+
+}
